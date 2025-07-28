1fcd9afcc6b8e5a2c96339823eb8cde66e7e7cfd
arpit <arpit@SCandCo>
2025-07-28 13:49:15 +0530

    second

--- tesrngexample/src/test/java/test.java
+++ tesrngexample/src/test/java/test.java
@@ -1,1 +1,23 @@
-it is check pull cite
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test {
+	
+	@Test(description="this is testcase1")
+	void LoginWithCorrectData()
+	{
+		System.out.println("in testcase1");
+		/*WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.automationexercise.com/login");
+		
+		WebElement username = driver.findElement(By.name("email"));
+		username.sendKeys("richa@gmail.com");		
+		driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]")).sendKeys("123456");
+		//username.clear();
+		driver.findElement(By.className("btn-default")).submit();
+		*/
+		
+	}
+}
+	
